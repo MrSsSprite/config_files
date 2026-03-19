d9698eab21b6614bea8c4650a914ea487f044a70
Archeus Lam <mrsssprite@gmail.com>
2026-03-20 00:49:17 +0800

    refactor: extract theme into a script

--- tmux/.config/tmux/tmux-status/right.sh
+++ tmux/.config/tmux/tmux-status/right.sh
@@ -1,62 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-theme="dawn"
-if [[ $theme == main ]]; then
-
-   thm_base="#191724";
-   thm_surface="#1f1d2e";
-   thm_overlay="#26233a";
-   thm_muted="#6e6a86";
-   thm_subtle="#908caa";
-   thm_text="#e0def4";
-   thm_love="#eb6f92";
-   thm_gold="#f6c177";
-   thm_rose="#ebbcba";
-   thm_pine="#31748f";
-   thm_foam="#9ccfd8";
-   thm_iris="#c4a7e7";
-   thm_hl_low="#21202e";
-   thm_hl_med="#403d52";
-   thm_hl_high="#524f67";
-
-elif [[ $theme == dawn ]]; then
-
-   thm_base="#faf4ed";
-   thm_surface="#fffaf3";
-   thm_overlay="#f2e9e1";
-   thm_muted="#9893a5";
-   thm_subtle="#797593";
-   thm_text="#575279";
-   thm_love="#b4637a";
-   thm_gold="#ea9d34";
-   thm_rose="#d7827e";
-   thm_pine="#286983";
-   thm_foam="#56949f";
-   thm_iris="#907aa9";
-   thm_hl_low="#f4ede8";
-   thm_hl_med="#dfdad9";
-   thm_hl_high="#cecacd";
-
-elif [[ $theme == moon ]]; then
-
-   thm_base="#232136";
-   thm_surface="#2a273f";
-   thm_overlay="#393552";
-   thm_muted="#6e6a86";
-   thm_subtle="#908caa";
-   thm_text="#e0def4";
-   thm_love="#eb6f92";
-   thm_gold="#f6c177";
-   thm_rose="#ea9a97";
-   thm_pine="#3e8fb0";
-   thm_foam="#9ccfd8";
-   thm_iris="#c4a7e7";
-   thm_hl_low="#2a283e";
-   thm_hl_med="#44415a";
-   thm_hl_high="#56526e";
-
-fi
+source ~/.config/tmux/theme.sh
+load_theme "dawn"
 
 separator=""
 right_cap="█"
