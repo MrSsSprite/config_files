a39c375ccca87f9f4e85d7739737a117ac97cd44
Archeus Lam <mrsssprite@gmail.com>
2026-04-04 03:21:11 +0800

    feat(picom): use new standard
    New picom standard moves most CLI flags into a config file

--- linux-gui/autostart.sh
+++ linux-gui/autostart.sh
@@ -2,8 +2,8 @@
 
 /bin/bash ./dwm-status.sh &
 /bin/bash ./wp-autochange.sh &
-picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-#picom -b
+#picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+picom -b
 /bin/bash ./startup-scripts.sh &
 #/bin/bash ~/scripts/setxmodmap-colemak.sh &
 #nm-applet &
